9e5808e95a4cc282a02b8113ebe41e6dc6e7b057
Peter Yakovlev <gorkiy@gmail.com>
2021-10-30 13:24:49 +0300

    Add metrics by dates range

--- stats.js
+++ stats.js
@@ -1,16 +1,16 @@
 import { uuid } from './config.js';
 
 function quantile(arr, q) {
-    const sorted = arr.sort((a, b) => a - b);
-    const pos = (sorted.length - 1) * q;
-    const base = Math.floor(pos);
-    const rest = pos - base;
+	const sorted = arr.sort((a, b) => a - b);
+	const pos = (sorted.length - 1) * q;
+	const base = Math.floor(pos);
+	const rest = pos - base;
 
-    if (sorted[base + 1] !== undefined) {
-        return Math.floor(sorted[base] + rest * (sorted[base + 1] - sorted[base]));
-    } else {
-        return Math.floor(sorted[base]);
-    }
+	if (sorted[base + 1] !== undefined) {
+		return Math.floor(sorted[base] + rest * (sorted[base + 1] - sorted[base]));
+	} else {
+		return Math.floor(sorted[base]);
+	}
 };
 
 function prepareData(result) {
@@ -23,7 +23,42 @@ function prepareData(result) {
 
 // TODO: реализовать
 // показать значение метрики за несколько день
-function showMetricByPeriod() {
+function addMetricByRange(data, page, name, dateFromString, dateToString) {
+	const sampleData = data.filter(item => {
+		const isCurrent = item.page == page && item.name == name;
+		const curDate = getTimestampFromSting(item.date);
+		const dateFrom = getTimestampFromSting(dateFromString);
+		const dateTo = getTimestampFromSting(dateToString);
+		dateTo.setHours(23);
+		dateTo.setMinutes(59);
+		dateTo.setSeconds(59);
+
+		const isValidDate = curDate.getTime() > dateFrom.getTime() && curDate.getTime() < dateTo.getTime();
+
+		return isCurrent && isValidDate;
+	}).map(item => item.value);
+
+	let result = {};
+
+	result.hits = sampleData.length;
+	result.p25 = quantile(sampleData, 0.25);
+	result.p50 = quantile(sampleData, 0.5);
+	result.p75 = quantile(sampleData, 0.75);
+	result.p95 = quantile(sampleData, 0.95);
+
+	return result;
+}
+
+function calcMetricByRange(data, page, dateFrom, dateTo) {
+	console.log(`All metrics from ${dateFrom} to ${dateTo}:`);
+	let table = {};
+
+	table.connect = addMetricByRange(data, page, 'connect', dateFrom, dateTo);
+	table.ttfb = addMetricByRange(data, page, 'ttfb', dateFrom, dateTo);
+	table.loaded = addMetricByRange(data, page, 'loaded', dateFrom, dateTo);
+	table.result = addMetricByRange(data, page, 'result', dateFrom, dateTo);
+
+	console.table(table);
 }
 
 // показать сессию пользователя
@@ -41,8 +76,8 @@ function compareMetric() {
 // добавить метрику за выбранный день
 function addMetricByDate(data, page, name, date) {
 	let sampleData = data
-					.filter(item => item.page == page && item.name == name && item.date == date)
-					.map(item => item.value);
+		.filter(item => item.page == page && item.name == name && item.date == date)
+		.map(item => item.value);
 
 	let result = {};
 
@@ -67,12 +102,19 @@ function calcMetricsByDate(data, page, date) {
 	console.table(table);
 };
 
+function getTimestampFromSting(dateString) {
+	const [year, month, day] = dateString.split('-');
+	return new Date(year, month - 1, day);
+}
+
 fetch(`https://shri.yandex/hw/stat/data?counterId=${uuid}`)
 	.then(res => res.json())
 	.then(result => {
 		let data = prepareData(result);
+		console.log('data: ', data);
 
 		calcMetricsByDate(data, 'index.html', '2021-10-30');
 
+		calcMetricByRange(data, 'index.html', '2021-10-29', '2021-10-30');
 		// добавить свои сценарии, реализовать функции выше
 	});
